9ca1de4eb67b7271069e713519de7dad43ef7835
Нагаев Алексей <nag495@yandex.ru>
2023-07-31 22:01:42 +0300

    9 - Переходим на express - базовое подключение

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import http from "http";
+import express from "express";
 
-const host = '127.0.0.1';
 const port = 8000
+const app = express() // Создаём сервер
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Connt-Typee', 'text/plain');
-    res.end('Привет!');
-});
+// Маршрутизация
+app.get('/hello', (req, res) => {
+    res.send('Привет!')
+})
 
-server.listen(port, host, () => {
-    console.log(`Сервер запущен на ${host}:${port}`);
+// Слушатель подключения сервера
+app.listen(port, () => {
+    console.log(`Сервер запущен на http://localhost:${port}`);
 })
